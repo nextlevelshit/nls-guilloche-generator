4617e9037447462687c8522466db7e31b9d6f7f5
Michael Czechowski <mail@dailysh.it>
2018-05-10 17:13:29 +0200

    capsulated to directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CanvasDirective } from './canvas/canvas.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/canvas/canvas.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCanvas]'
+})
+export class CanvasDirective {
+
+  constructor() { }
+
+}
