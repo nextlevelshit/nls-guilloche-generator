86eab2066e26cd50f71d4638911e5318712a9be0
Michael Czechowski <mail@dailysh.it>
2018-06-27 19:27:49 +0200

    added library project

---
+++ projects/nls-guilloche/src/lib/nls-guilloche.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-nls-guilloche',
+  template: `
+    <p>
+      nls-guilloche works!
+    </p>
+  `,
+  styles: []
+})
+export class NlsGuillocheComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/nls-guilloche/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of nls-guilloche
+ */
+
+export * from './lib/nls-guilloche.service';
+export * from './lib/nls-guilloche.component';
+export * from './lib/nls-guilloche.module';
